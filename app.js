f59fcb853b5df2517c414daece439781b9c13958
Prathvika Krishna Moger <mogerprathvika@gmail.com>
2026-07-05 04:45:19 +0530

    Fix render deployment

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ const store = MongoStore.create({
   },
   touchAfter: 24 * 3600,
 });
-store.on("error", () => {
+store.on("error", (err) => {
   console.log("ERROR in MONGO SESSION STORE", err);
 });
 const sessionOptions = {
@@ -91,6 +91,9 @@ app.use((err, req, res, next) => {
 
   res.status(statusCode).render("error.ejs", { message });
 });
-app.listen(8080, () => {
-  console.log("server is listening to port 8080");
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
